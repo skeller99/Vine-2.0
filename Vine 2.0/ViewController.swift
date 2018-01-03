7f27f20a5fe983b8d15bbceefd6470077caf8e2d
Sophia Grace Keller <sophiakeller@Christophers-MacBook-Air.local>
2018-01-03 10:07:09 -0500

    First committed Vine 2.0

--- Vine 2.0/ViewController.swift
+++ Vine 2.0/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        var backgroundColor = UIColor.red
+        self.view.backgroundColor = backgroundColor
     }
 
     override func didReceiveMemoryWarning() {
