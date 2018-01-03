9dd005cd85feebc1ba6fb136762073b6d069d7e6
Sophia Grace Keller <sophiakeller@Christophers-MacBook-Air.local>
2018-01-03 10:19:30 -0500

    Daryl Dixon is my fav

--- Vine 2.0/ViewController.swift
+++ Vine 2.0/ViewController.swift
@@ -12,18 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var backgroundColor = UIColor.red
+        
+        let backgroundColor = UIColor.red
         self.view.backgroundColor = backgroundColor
+        
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor = UIColor.blue
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
+        }
+
 // testing
 
-}
+
 
