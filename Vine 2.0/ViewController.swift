0befeb854005a3b4c43c2c011b7068efabe1f608
Sophia Grace Keller <sophiakeller@Christophers-MacBook-Air.local>
2018-01-03 09:50:46 -0500

    Initial Commit

---
+++ Vine 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Vine 2.0
+//
+//  Created by Sophia Grace Keller on 1/3/18.
+//  Copyright © 2018 Product1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
