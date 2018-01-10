129279c0e2906e4d75452c2353e96aa1eb904438
Sophia Grace Keller <sophiakeller@Christophers-MacBook-Air.local>
2018-01-10 11:06:54 -0500

    fkgfghj

--- Vine 2.0/ViewController.swift
+++ Vine 2.0/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    var audioPlayer:AVAudioPlayer = AVAudioPlayer()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,10 +20,21 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
         
+//        how to input sound files
+        //i smell like beef, look at all those chickens, croissant, hurricane katrina, damn daniel, i go to sleep now, hi welcome to chili's, ginger kid in car, this is my jam
         
-         
     }
-
+  
+    func playAudioFIle() {
+    
+    if let path = Bundle.main.path(forResource: "MattCorbyResolution.mp3", ofType: nil){
+        let url = URL(fileURLWithPath: path)
+        audioPlayer = try! AVAudioPlayer(contentsOf: url)
+        audioPlayer.prepareToPlay()
+        audioPlayer.play()
+        }
+        
+    }
    
         }
 
