eb263c0123937e65ef444f370f453b2e786ae34d
cdaly17 <cdaly1724@gmail.com>
2018-01-22 10:12:08 -0500

    i hate gifs

--- Vine 2.0/ViewController.swift
+++ Vine 2.0/ViewController.swift
@@ -17,25 +17,25 @@ class ViewController: UIViewController{
     var audioPlayer:AVAudioPlayer = AVAudioPlayer()
    
     
-    @IBOutlet weak var gifOne: UIImageView!
-   
-    @IBOutlet weak var gifTwo: UIImageView!
-    
-    @IBOutlet weak var gifThree: UIImageView!
-    
-    @IBOutlet weak var gifFour: UIImageView!
+//    @IBOutlet weak var gifOne: UIImageView!
+//   
+//    @IBOutlet weak var gifTwo: UIImageView!
+//    
+//    @IBOutlet weak var gifThree: UIImageView!
+//    
+//    @IBOutlet weak var gifFour: UIImageView!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        gifOne.loadGif(name: "giffy")
+//          gifOne.image = UIImage.gif(name: "jeremy")
         
-        gifTwo.loadGif(name: "theregif")
-        
-        gifThree.loadGif(name: "gif")
-        
-        gifFour.loadGif(name: "gifGif")
+//        gifTwo.loadGif(name: "theregif")
+//
+//        gifThree.loadGif(name: "gif")
+//
+//        gifFour.loadGif(name: "gifGif")
         
         myLabel.textColor = UIColor.white
     }
---
+++ Vine 2.0/ViewController2.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController2.swift
+//  Vine 2.0
+//
+//  Created by  on 1/22/18.
+//  Copyright © 2018 Product1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    
+    @IBOutlet weak var gifOne: UIImageView!
+    
+    @IBOutlet weak var gifTwo: UIImageView!
+    
+    @IBOutlet weak var gifThree: UIImageView!
+    
+    @IBOutlet weak var gifFour: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        gifOne.loadGif(name: "gif")
+        
+        gifTwo.loadGif(name: "giffy")
+        
+        gifThree.loadGif(name: "theregif")
+        
+        gifFour.loadGif(name: "gifGif")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
